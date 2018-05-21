4b8af478e7a95625fe00ae9ff1cc16d94174c3da
Diajeta Ahante <diajeta@ahante.com.ng>
2018-05-21 01:30:58 -0500

    first commit

---
+++ src/android/Calculator.java
@@ -0,0 +1,80 @@
+package cordova-plugin-calculator;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Calculator extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if(action.equals("add")){
+            this.add(args, callbackContext);
+            return true;
+        }else if(action.equals("multiply")){
+            this.multiply(args, callbackContext);
+            return true;
+        }else if(action.equals("subtract")){
+            this.subtract(args, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void add(JSONArray args, CallbackContext callback){
+        if(args != null){
+            try{
+
+                int p1 = Integer.paseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.paseInt(args.getJSONObject(0).getString("param2"));
+
+                callback.success(""+ (p1+p2));
+
+            }catch(Exception ex){
+                callback.error("Something whent wrong " + ex);
+            }
+        }else{
+            callback.error("Please do not enter null values");
+        }
+    }
+
+    private void multiply(JSONArray args, CallbackContext callback){
+        if(args != null){
+            try{
+
+                int p1 = Integer.paseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.paseInt(args.getJSONObject(0).getString("param2"));
+
+                callback.success(""+ (p1*p2));
+
+            }catch(Exception ex){
+                callback.error("Something whent wrong " + ex);
+            }
+        }else{
+            callback.error("Please do not enter null values");
+        }
+    }
+
+    private void subtract(JSONArray args, CallbackContext callback){
+        if(args != null){
+            try{
+
+                int p1 = Integer.paseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.paseInt(args.getJSONObject(0).getString("param2"));
+
+                callback.success(""+ (p1-p2));
+
+            }catch(Exception ex){
+                callback.error("Something whent wrong " + ex);
+            }
+        }else{
+            callback.error("Please do not enter null values");
+        }
+    }
+}
